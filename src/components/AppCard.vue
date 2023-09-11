01d99dc2c3c26a66cab114adb85b7abe1a4653f5
CarloCassini <c.cassini87@gmail.com>
2023-09-11 17:16:30 +0200

    start component card

---
+++ src/components/AppCard.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  props: {
+    thumb: String,
+    serie: String,
+  },
+};
+</script>
+
+<template>
+  <section></section>
+</template>
+
+<style lang="scss" scoped></style>
